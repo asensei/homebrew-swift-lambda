e99238214e06aa7fd9f7e4c165210198d1feffbf
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2019-06-14 16:19:07 +0100

    pre build options

--- Formula/swift-lambda.rb
+++ Formula/swift-lambda.rb
@@ -1,7 +1,7 @@
 class SwiftLambda < Formula
   desc "A command line interface that provides shortcuts and assistance for creating and packaging AWS Lambda Swift functions."
   homepage "https://github.com/asensei/homebrew-swift-lambda"
-  version "0.1.0"
+  version "0.2.0"
   url "https://github.com/asensei/homebrew-swift-lambda.git", :using => :git, :tag => version.to_s
 
   depends_on :xcode => "10.2"
